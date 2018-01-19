f1efcfc498707bbb5cabd7e955cdcbf483b187ff
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-19 18:50:03 +0300

    Setup the tolbar and navigation drawer items

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+        theme: {
+                primary: '#3f51b5',
+                secondary: '#b0bec5',
+                accent: '#8c9eff',
+                error: '#b71c1c',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107'
+        }
+}),
 
 Vue.config.productionTip = false
 
